545292d83ca9e16a45c8e955cc0ced7df947c255
Alfred Gamulo <Alfredmarc.Gamulo@capitalone.com>
2017-03-28 11:25:41 -0400

    implementing python for lazy spelling bee challenge

--- python/lazy_spelling_bee/main.py
+++ python/lazy_spelling_bee/main.py
@@ -6,8 +6,34 @@
 # ===============================
 #
 
+mod = 1000000007
+
 def do_jam(input):
-    return input
+    slen = len(input)
+    if(slen == 1):
+        return 1
+    if(slen == 2):
+        if(input[0] == input[1]):
+            return 1
+        else:
+            return 4
+
+    ans = 1
+
+    if(input[0] != input[1]):
+        ans = (ans * 2) % mod
+    if(input[-1] != input[-2]):
+        ans = (ans * 2) % mod
+
+    for i in xrange(1, slen - 1):
+        c = 1
+        if(input[i] != input[i-1]):
+            c+=1
+        if((input[i+1] != input[i-1]) and (input[i] != input[i+1])):
+            c+=1
+        ans = (ans * c) % mod
+
+    return ans % mod
 
 
 ########
